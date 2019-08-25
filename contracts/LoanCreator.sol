55063b4f58110da0f8a4cd98ae27a5939df31a58
livingrock7 <chiragtitiya6893@gmail.com>
2019-08-25 21:50:18 +0530

    Update LoanCreator.sol
    updated tested file
    changed contract names

--- contracts/LoanCreator.sol
+++ contracts/LoanCreator.sol
@@ -2,13 +2,13 @@ pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
-import "./BLoanContract.sol";
+import "./LoanContract.sol";
 
-contract Finocial is Ownable, Pausable {
+contract LoanCreator is Ownable, Pausable {
 
-    address[] public loans;
-    
-    enum LoanStatus {
+  address[] public loans;
+  
+  enum LoanStatus {
         OFFER,
         REQUEST,
         ACTIVE,
@@ -17,39 +17,40 @@ contract Finocial is Ownable, Pausable {
         DEFAULT
     }
 
-    constructor() public {
 
-    }
+ constructor() public {
 
-    event LoanOfferCreated(address, address);
-    event LoanRequestCreated(address, address);
+ }
 
-    // parameters and attributes will change depending on changes in loancontract
-    function createNewLoanOffer(uint256 _loanAmount, uint128 _duration, string _acceptedCollateralsMetadata) public returns(address _loanContractAddress) {
+ event LoanOfferCreated(address, address);
+ event LoanRequestCreated(address, address);
 
-            _loanContractAddress = address (new BLoanContract(_loanAmount, _duration, _acceptedCollateralsMetadata, 0, address(0), 0, 0, 0, address(0), msg.sender, LoanStatus.OFFER));
+ // parameters and attributes will change depending on changes in loancontract
+ function createNewLoanOffer(uint256 _loanAmount, uint128 _duration, string memory _acceptedCollateralsMetadata) public returns(address _loanContractAddress) {
 
-            loans.push(_loanContractAddress);
+         _loanContractAddress = address (new LoanContract(_loanAmount, _duration, _acceptedCollateralsMetadata, 0, address(0), 0, 0, 0, address(0), msg.sender, LoanStatus.OFFER));
 
-            emit LoanOfferCreated(msg.sender, _loanContractAddress);
+         loans.push(_loanContractAddress);
 
-            return _loanContractAddress;
-    }
-    
-    function createNewLoanRequest(uint256 _loanAmount, uint128 _duration, uint256 _interest, address _collateralAddress, uint256 _collateralAmount, uint256 _collateralPriceInETH) 
-    public returns(address _loanContractAddress) {
+         emit LoanOfferCreated(msg.sender, _loanContractAddress);
 
-            _loanContractAddress = address (new BLoanContract(_loanAmount, _duration, "", _interest, _collateralAddress, _collateralAmount, _collateralPriceInETH, 50, msg.sender, address(0), LoanStatus.REQUEST));
+         return _loanContractAddress;
+ }
 
-            loans.push(_loanContractAddress);
+ function createNewLoanRequest(uint256 _loanAmount, uint128 _duration, uint256 _interest, address _collateralAddress, uint256 _collateralAmount, uint256 _collateralPriceInETH)
+ public returns(address _loanContractAddress) {
 
-            emit LoanRequestCreated(msg.sender, _loanContractAddress);
+         _loanContractAddress = address (new LoanContract(_loanAmount, _duration, "", _interest, _collateralAddress, _collateralAmount, _collateralPriceInETH, 50, msg.sender, address(0), LoanStatus.REQUEST));
 
-            return _loanContractAddress;
-    }
+         loans.push(_loanContractAddress);
 
-    function getAllLoans() public view returns(address[] memory){
-        return loans;
-    }
+         emit LoanRequestCreated(msg.sender, _loanContractAddress);
+
+         return _loanContractAddress;
+ }
+
+ function getAllLoans() public view returns(address[] memory){
+     return loans;
+ }
 
 }
